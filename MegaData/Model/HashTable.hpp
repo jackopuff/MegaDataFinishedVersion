af966c5affe2ce3250626d0e31a4d0ffa7a66219
jackopuff <jackson@carterrealm.com>
2017-05-25 07:16:24 -0600

    added constructor for hashNode

---
+++ MegaData/Model/HashTable.hpp
@@ -0,0 +1,15 @@
+/*
+ * HashTable.hpp
+ *
+ *  Created on: May 25, 2017
+ *      Author: jackson
+ */
+
+#ifndef MODEL_HASHTABLE_HPP_
+#define MODEL_HASHTABLE_HPP_
+
+
+
+
+
+#endif /* MODEL_HASHTABLE_HPP_ */
